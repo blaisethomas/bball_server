144428b71e44bbd8ac5c61f8fe5811b4b56a0d22
Blaise Thomas <blaisegthomas@gmail.com>
2016-12-02 12:05:44 -0800

    adds mongoose

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
-var port = 3000;
+var mongoose = require('mongoose');
+var mongoUri = process.env.MONGOLAB_URI || "mongodb://ztebball:production@ds119738.mlab.com:19738/zte-bball-game";
+var port = process.env.PORT || 3000;
 
-app.use('/', function(req, res ) {
+mongoose.connect(mongoUri);
+
+app.use('/scores', function(req, res ) {
+
     res.json({"hello" : "monkey"});
 })
 app.listen(port, function(){
