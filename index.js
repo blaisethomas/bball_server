9f35c0fa2a871785827ea8b209efa4e3db287d58
Blaise Thomas <blaisegthomas@gmail.com>
2016-12-02 15:35:47 -0800

    whitespace

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 //app.use(express.static(path.join(__dirname, 'public')))
 
-
 app.use('/players', playerRoutes );
-
 
 app.listen(port, function(){
     console.log("ZTE Basketball server side magic is happening on PORT " + port);
