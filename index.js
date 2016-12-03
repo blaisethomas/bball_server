864eb1df3ac71727df82c83d2e2f32a7a0d4793e
Blaise Thomas <blaisegthomas@gmail.com>
2016-12-03 15:57:22 -0800

    logger

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express 	= require('express');
 var app 		= express();
 var mongoose 	= require('mongoose');
 var bodyParser	= require('body-parser');
+var morgan 		= require('morgan');
 var playerRoutes = require ('./scores/playerRouter');
 var mongoUri 	= process.env.MONGOLAB_URI || "mongodb://ztebball:production@ds119738.mlab.com:19738/zte-bball-game";
 var port 		= process.env.PORT || 3000;
@@ -11,10 +12,11 @@ mongoose.connect(mongoUri);
 //app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 //app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/players', playerRoutes );
 
 app.listen(port, function(){
-    console.log("ZTE Basketball server side magic is happening on PORT " + port);
+    console.log("ZTE Basketball " + port);
 })
